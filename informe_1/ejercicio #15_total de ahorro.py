ca12bdde54d19d7715498652e5399aac0512ceeb
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 09:24:16 -0500

    Add files via upload

---
+++ informe_1/ejercicio #15_total de ahorro.py
@@ -0,0 +1,8 @@
+#este programa calcula el total de ahorros y el promedio de ahorros por persona
+misahorros = 100
+susahorros = 150
+ahorrostotales = misahorros + susahorros
+promedioahorroporpersona = ahorrostotales / 2
+
+print(ahorrostotales)
+print(promedioahorroporpersona)
