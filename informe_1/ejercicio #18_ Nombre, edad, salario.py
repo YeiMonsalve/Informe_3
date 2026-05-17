137317b89f7249ede8904ede3985501c633264ed
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 09:31:19 -0500

    Add files via upload

---
+++ informe_1/ejercicio #18_ Nombre, edad, salario.py
@@ -0,0 +1,13 @@
+#este programa solicita al usuario su nombre, edad, salario, materia y nota de la materia,
+# y luego imprime esta información en pantalla.
+nombre = str(input("Ingrese su nombre: "))
+edad = int(input("Ingrese su edad: "))
+salario = int(input("Ingrese su salario: "))
+materia = input("Ingrese el nombre de la materia que estás cursando: ")
+notamateria = int(input("Ingrese la nota de la materia: "))
+
+print("Su nombre es:", nombre)
+print("Su edad es:", edad)
+print("Su salario es:", salario)
+print("Su materia es:", materia)
+print("Su nota materia es:", notamateria)
