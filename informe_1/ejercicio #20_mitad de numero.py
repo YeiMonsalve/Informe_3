b5180da5914d9cb1e6abf67178c06bde2216cfae
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 09:34:29 -0500

    Add files via upload

---
+++ informe_1/ejercicio #20_mitad de numero.py
@@ -0,0 +1,8 @@
+# Solicitar un número al usuario. 
+# Calcular y mostrar en terminal el valor de la mitad de este número.
+
+num = int(input("Ingrese un número: "))
+
+mitadNum = num // 2
+
+print("La mitad del número", num, "es:", mitadNum)
