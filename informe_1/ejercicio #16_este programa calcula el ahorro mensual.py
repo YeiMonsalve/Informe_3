1aeafd7b7d56447a2b16a883967c470a9acec61a
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 09:25:43 -0500

    Add files via upload

---
+++ informe_1/ejercicio #16_este programa calcula el ahorro mensual.py
@@ -0,0 +1,10 @@
+#este programa calcula el ahorro mensual a partir del ahorro diario
+# ahorro diario
+ahorrodiario = 100
+
+# operación para obtener el ahorro mensual
+ahorromensualesperado = ahorrodiario * 30
+
+# se imprime el resultado
+print(ahorrodiario)
+print(ahorromensualesperado)
