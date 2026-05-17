26806b5c5e1c4cc0b97d5ad4a3f92f7f02dcd8bf
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 09:19:37 -0500

    Add files via upload

---
+++ informe_1/ejercicio #13_Nombre, edad, estatura.py
@@ -0,0 +1,8 @@
+#este ejercicio pide nombre, edad y estatura de una persona
+nombre = "hola daniela"
+edad = 22
+estatura = 1.52
+
+print(type(nombre), nombre)
+print(type(edad), edad)
+print(type(estatura), estatura)
