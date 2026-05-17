fb9b19d1da8d2f98f2c61a56212d58b69efd24ee
Daniela-Misas-Amaya <daniela.misas@correo.tdea.edu.co>
2026-05-17 08:50:04 -0500

    Add files via upload

---
+++ informe_1/ejercicio #1 calificaciones.py
@@ -0,0 +1,12 @@
+# calcula si un alumno esta aprobado o reprobado con base en su calificacion
+
+# Algoritmo calificaciones
+
+calificacion = 0
+
+calificacion = float(input("Ingrese la calificacion del alumno: "))
+
+if calificacion >= 8:
+    print("El alumno esta APROBADO")
+else:
+    print("El alumno esta REPROBADO")
