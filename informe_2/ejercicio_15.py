bc9eceb97ff8ba2fed1d9fc41e0530dbd940861c
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:32:43 -0500

    Update ejercicio_15.py

--- informe_2/ejercicio_15.py
+++ informe_2/ejercicio_15.py
@@ -1,1 +1,11 @@
-
+# programa que imprime las primeras N potencias de 2 requeridas por el usuario 
+# y calcula su promedio 
+N= int (input ("ingrese la cantidad de potencias que desea: "))
+i=0
+suma=0
+while i<N: 
+  potencia=2**i
+  print ("2 ^",i, "=" , potencia)
+   suma +=potencia 
+   i+= suma /N 
+print ("el promedio de las potencias es de: ",promedio)
