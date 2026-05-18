9b5b18209a06ee38da6416caa8160bad34732915
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:12:23 -0500

    Update and rename ejercicio_4,py to ejercicio_4.py

---
+++ informe_2/ejercicio_4.py
@@ -0,0 +1,10 @@
+#Ejercicio Condicional solicitar Edad y Nombre - Mostrar mensaje personalizado donde se diga si puede o no votar
+
+nombre= str (input ("ingrese su nombre: "))
+edad= int (input ("ingrese su edad : "))
+
+if edad >=18:
+  print ("usted puede votar")
+else :
+  print ("usted NO puede votar ")
+  
