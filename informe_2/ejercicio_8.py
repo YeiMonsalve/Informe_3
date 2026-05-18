c6f806128429fb3bd92ca055096b6f26426f5ff5
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:34:18 -0500

    Update ejercicio_8.py

--- informe_2/ejercicio_8.py
+++ informe_2/ejercicio_8.py
@@ -1,1 +1,15 @@
+#ejercicio para determinar el costo de las flores segun la cantidad que se vaya a comprar 
 
+cantidadflores=int(input("ingrese la cantidad de flores a comprar "))
+if (cantidadflores>=10):
+  preciototal=cantidadflores*8
+  print ("el total a pagar es", +str(preciototal))
+elif (cantidadflores>=):
+  preciototal= cantidadflores*10
+  print ("el total a pagar es: ",+str (preciototal) )
+elif (preciototal==3):
+  print ("no debe pagar nada, las flores son gratis ")
+else :
+  preciototal=cantidadflores*15
+  print ("el precio a pagar es de : ",+str (preciototal))
+print ("fin del algoritmo")  
