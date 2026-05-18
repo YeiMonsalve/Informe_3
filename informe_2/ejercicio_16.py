e4a47162ade214705c2531a6c489c6432e2ab0da
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:44:00 -0500

    Update ejercicio_16.py

--- informe_2/ejercicio_16.py
+++ informe_2/ejercicio_16.py
@@ -1,1 +1,19 @@
+#ejercicio que muestra un ingreso con usuario y contraseña 
 
+usuario_correcto=("admin")
+contraseña_correcto =("123")
+
+opcion =0
+while opcion !=2: 
+  print (" Login ")
+  print ("1. ingrese las credenciales ")
+  print ("2. salir ")
+
+  opcion = int (input ("seleccione una opcion "))
+   if opcion ==1: 
+     usuario= str(input("ingrese el usuario: "))
+     contraseña= str(input("ingrese la contraseña:"))
+     if usuario == usuario_correcto and contraseña==contraseña_correcto:
+       print ("inicio de sesion valido")
+     else : 
+       print ("inicio de sesion invalido ")
