0b7afc957f014a7734a260a30358dfb781161f15
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:18:17 -0500

    Update ejercicio_13.py

--- informe_2/ejercicio_13.py
+++ informe_2/ejercicio_13.py
@@ -1,1 +1,11 @@
+#diseñe  un programa que pida numeros enteros al usuario
+#hasta que ingrese el 0, para cada entradad publique si es par o impar 
 
+numero= int (input ("ingrese el numero entero : (para finalizar ingrese el 0)"))
+while numero!=0: 
+  if numero %2==0: 
+    print (f"el numero{numero} es par ")
+  else: 
+    print (f"el numero {numero} es impar ")
+   numero = int (input (ingrese otro numero: ))
+print ("programa finalizado ")
