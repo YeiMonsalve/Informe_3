41e8068ea86a37f03a026ed268a5ee2197a71bee
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:45:28 -0500

    Update ejercicio_9.py

--- informe_2/ejercicio_9.py
+++ informe_2/ejercicio_9.py
@@ -1,1 +1,9 @@
+#ejercicio para dterminar que generacion eres segun tu fecha de nacimiento
 
+año = int (input ("ingrese su año de nacimiento"))
+if (año >=1994 and año <=2010):
+  print ("eres generacion Z")
+elif (año >=1991 and año <=1993):
+   print ("eres milenial ")
+else : 
+  print ("eres de otra generacion")
