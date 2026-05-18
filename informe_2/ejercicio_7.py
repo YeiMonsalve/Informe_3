fe7f498a3601d6ec59bec86965556c87f01ae810
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:27:24 -0500

    Update ejercicio_7.py

--- informe_2/ejercicio_7.py
+++ informe_2/ejercicio_7.py
@@ -1,1 +1,12 @@
+#ejercicio imprima la temperatura y dependiento como este comprar
+#helado,chocolate o jugo de naranja 
 
+temperatura= float (input("ingrese la temperatura "))
+
+if (temperatura>27):
+ print ("comprar helado")
+elif ("temperatura<15"):
+ print ("comprar chocolate")
+else: 
+ print ("comprar jugo de naranja ")
+print ("fin del programa ")
