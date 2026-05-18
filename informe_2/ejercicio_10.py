3bd2873c110f7d8148121b246fe3958832bc302c
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:06:43 -0500

    ejercicio_20.py to ejercicio_10.py

---
+++ informe_2/ejercicio_10.py
@@ -0,0 +1,13 @@
+#ejercicio que indica el tipo de musica y el año de nacimiento 
+
+genero = str (input ("ingrese el genero de musica "))
+
+if (genero=="electronica" or genero=="pop"):
+año = int (input ("ingrese su año de nacimiento :"))
+   if (año >2000 and genero=="pop"):
+     print ("tengo la camisa negra ")
+    else : 
+   print ("daft punk por siempre ")
+else : 
+print ("los unicos generos buenos son electronica y pop")
+print ("fin del algoritmo")
