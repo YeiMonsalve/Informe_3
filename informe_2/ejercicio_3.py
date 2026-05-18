f82d8b20ed53d405dac6076df9a129848144a49e
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:07:10 -0500

    Update ejercicio_3.py

--- informe_2/ejercicio_3.py
+++ informe_2/ejercicio_3.py
@@ -1,1 +1,15 @@
+#leer 2 números y revisar si uno es mayor que el otro. Si se cumple lo anterior imprimir si el segundo es mayor dos o más veces que el primero. 
 
+
+num1= int (input ("ingrese su primer numero: "))
+num2= int (input ("ingrese su segundo  numero: "))
+if num2>num1:
+  if num2==*num1:
+   print ("el segundo numero es el doble del primero ")
+  elif num2> 2*num1:
+    print ("el segundo numero es mas de dos veces mayor que el primero ")
+  else :
+     print ("el segundo numero es mayor pero no el doble ")
+else : 
+  print ("el segundo numero es menor que el primero")
+print ("fin del algoritmo")
