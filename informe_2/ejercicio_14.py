e8687d0a4472204753d61a4c7e788d2bb23e0ad8
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:21:34 -0500

    Update ejercicio_14.py

--- informe_2/ejercicio_14.py
+++ informe_2/ejercicio_14.py
@@ -1,1 +1,11 @@
+#diseñar un progrma que muestre las tablas de multiplicar de un numero N ingresado por el usuario 
+#solo del 1 al 10 
+
+N=int (input ("ingrese un numero "))
+
+i=1
+while i<=10 : 
+  print (N, "x",i, "=", N*i)
+i+=1
+
 
