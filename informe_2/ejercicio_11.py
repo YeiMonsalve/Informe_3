f415baaf07946977a7ed9996f7e6dae243b167cf
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 15:09:21 -0500

    Update ejercicio_11.py

--- informe_2/ejercicio_11.py
+++ informe_2/ejercicio_11.py
@@ -1,1 +1,12 @@
+#ejercicio de ciclos while 
 
+N = int (input ("ingrese el numero "))
+
+i= 1
+suma =0
+while i<=N: 
+  print (i)
+  suma +=1
+  i+=1
+ print ("la suma de los numeros es: ", suma) 
+  
