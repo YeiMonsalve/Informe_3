a569e030dde6f8285fbf9b132bec07fcc6e31cf0
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:16:52 -0500

    Update ejercicio_5.py

--- informe_2/ejercicio_5.py
+++ informe_2/ejercicio_5.py
@@ -1,1 +1,11 @@
+#ejercicio lado cuadrado 
 
+ladocua= float (input("ingrese el lado del cuadrado "))
+if (ladocua>0):
+  areacuadrado= ladocua*ladocua
+  print ("el area del cuadrado es:", +str(areacuadrado))
+else: 
+  print ("el lado del cuadrado no puede ser igual o menor a cero")
+
+print ("fin del algoritmo")
+  
