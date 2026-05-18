3512deb5b46c3760a7b7ff7213cde7e5deaaa5d8
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 14:22:57 -0500

    Update ejercicio_6.py

--- informe_2/ejercicio_6.py
+++ informe_2/ejercicio_6.py
@@ -1,1 +1,11 @@
+#ejercicio de votos para un partido 
+#determinar si adquieren la curul o si se queman 
 
+votosvalidos= int (input("ingrese los votos validos: "))
+votospartido=int (input("ingrese los votos de su partido"))
+umbral= votosvalidos*0.03
+if (votospartido>umbral):
+  print("tu partido tendra curules ")
+
+else : 
+  print ("se quemaron")
