2ab4848a32373dad8833f91fa5c3162d2b69e9a6
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-18 13:53:50 -0500

    Update exercise_1.py

---
+++ informe_2/exercise_1.py
@@ -0,0 +1,8 @@
+#ejercicio para medir la temperatura, y comprar helado 
+
+temp= int (input("cual es la temperatura actual"))
+dinero = int (input("ingrese el dinero "))
+
+if (temp>=27) and (dinero >=5): 
+  print ("comprar helado ")
+print ("fin del programa ")
