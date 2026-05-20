3e91c6975b9e0107bbd5cd4a59c41327848d8bae
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-19 19:36:15 -0500

    Update ejercicio_10.py

--- informe_2/ejercicio_10.py
+++ informe_2/ejercicio_10.py
@@ -3,11 +3,11 @@
 genero = str (input ("ingrese el genero de musica "))
 
 if (genero=="electronica" or genero=="pop"):
-año = int (input ("ingrese su año de nacimiento :"))
+   año = int (input ("ingrese su año de nacimiento :"))
    if (año >2000 and genero=="pop"):
      print ("tengo la camisa negra ")
-    else : 
-   print ("daft punk por siempre ")
-else : 
-print ("los unicos generos buenos son electronica y pop")
+   else: 
+    print ("daft punk por siempre ")
+else :  
+ print ("los unicos generos buenos son electronica y pop")
 print ("fin del algoritmo")
