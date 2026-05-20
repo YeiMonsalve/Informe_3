55f82ec35b1c5c0f1d1fe6d6dd7fdcefd84d4912
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-19 19:32:26 -0500

    Update ejercico_12.py

--- informe_2/ejercico_12.py
+++ informe_2/ejercico_12.py
@@ -1,6 +1,9 @@
 #jercicio while propuesto en clase : Solicitar un número entero hasta que esta que este 
-numero= int (input ("ingrese el numero entero "))
- while numero%11 !=0:
-   print ("el numero no es multiplo de 11")
-   numero = ("el numero ", numero, "si es multiplo de 11")
+numero = int(input("Ingrese un número entero: "))
+
+while numero % 11 != 0:
+    print("El número no es múltiplo de 11.")
+    numero = int(input("Ingrese otro número: "))
+
+print("El número es múltiplo de 11.")
    
