e41be1bb8a44c256f12adb8182bef07f972f3f1a
nymanuel-lab <nymanuelpa@gmail.com>
2026-05-19 19:18:17 -0500

    Update ejercicio_3.py

--- informe_2/ejercicio_3.py
+++ informe_2/ejercicio_3.py
@@ -4,7 +4,7 @@
 num1= int (input ("ingrese su primer numero: "))
 num2= int (input ("ingrese su segundo  numero: "))
 if num2>num1:
-  if num2==*num1:
+  if num2== 2*num1:
    print ("el segundo numero es el doble del primero ")
   elif num2> 2*num1:
     print ("el segundo numero es mas de dos veces mayor que el primero ")
